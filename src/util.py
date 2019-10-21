116cd658e85bb386de58a1b5733d54f5d114f446
William Yang <williamyang2017@gmail.com>
2019-10-21 21:41:10 +1100

    Added solver

---
+++ src/util.py
@@ -0,0 +1,45 @@
+from .word_tree import WordTree
+ 
+def load_tree(filepath):
+    tree = WordTree()
+    with open(filepath, "r") as file:
+        for i, word in enumerate(file.readlines()):
+            word = word.strip()
+            tree.add_word(word)
+            print(f"{i} {word}", end="\r")
+        print()
+
+    return tree
+
+def load_bounding_boxes(filepath):
+    bounding_boxes = {
+        'characters': [],
+        'values': [],
+        'bonuses': []
+    }
+
+    current_boxes = None
+
+    begin_header = "#begin "
+    end_footer = "#end"
+
+    with open(filepath, "r") as file:
+        for line in file.readlines():
+            if line.startswith(begin_header):
+                read_mode = line[len(begin_header):]
+                read_mode = read_mode.lower().strip()
+                if read_mode in bounding_boxes:
+                    current_boxes = bounding_boxes[read_mode]
+                continue
+        
+            if line.startswith(end_footer):
+                current_boxes = None
+                continue
+                
+            if current_boxes is not None:
+                bounding_box = map(int, line.strip().split(' '))
+                bounding_box = tuple(bounding_box)
+                current_boxes.append(bounding_box)
+    
+    return bounding_boxes
+                
