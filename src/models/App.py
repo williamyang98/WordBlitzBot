2c7eb77256b69b61cd18b6a22285b38add03635d
William Yang <williamyang2017@gmail.com>
2019-12-21 19:55:31 +1100

    Instantiation of all models done in app

---
+++ src/models/App.py
@@ -0,0 +1,33 @@
+from .Matrix import Matrix
+from .Dictionary import Dictionary, DictionarySerialiser
+from .Exporter import Exporter
+from .Preview import Preview
+from .Solver import Solver
+from .Tracer import Tracer
+from .Analyser import Analyser
+
+from src.util import load_bounding_boxes
+from .ScreenRect import ScreenRect
+
+class App:
+    def __init__(self):
+        self.matrix = Matrix()
+
+        self.dictionary_loader = DictionarySerialiser()
+        self.dictionary = self.dictionary_loader.load("assets/dictionaries/dictionary.pickle")
+
+        bounding_boxes = load_bounding_boxes("assets/bounding_boxes.txt")
+        screen_rect = (526, 422, 438, 440)
+        screen_rect = ScreenRect(screen_rect)
+
+        self.preview = Preview(bounding_boxes, screen_rect)
+        self.solver = Solver(self.dictionary)
+        self.analyser = Analyser(self.preview, self.matrix)
+
+        self.tracer = Tracer(self.solver, self.preview, self.matrix)
+        self.exporter = Exporter(self.tracer, self.preview, self.matrix)
+
+
+
+
+
